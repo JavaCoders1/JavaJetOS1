eb56b1b303071b17492178d5ee2cf35efbf239f3
AFK190 <aayush.alimchandani@gmail.com>
2023-04-10 22:36:00 +0530

    e

---
+++ UnderDevelopment/AgeCalc/AgeCalc.java
@@ -0,0 +1,28 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+
+public class AgeCalc{
+    public static void main(String[] args)
+    {
+        LocalDateTime now = LocalDateTime.now();
+        Scanner sc=new Scanner(System.in);
+        String DOB;
+        System.out.println("Please enter your date of birth in the format [DD-MM-YYYY]: ")
+        DOB = sc.NextLine()
+        String parts = DOB.split()
+        int DateOfB = Integer.parseint(parts[0])
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
