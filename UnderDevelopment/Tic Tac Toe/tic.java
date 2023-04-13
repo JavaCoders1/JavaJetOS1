db7aa118c338db949291d71889143813a51e690c
AFK1903 <130341337+AFK1903@users.noreply.github.com>
2023-04-13 06:18:34 +0000

    e

--- UnderDevelopment/Tic Tac Toe/tic.java
+++ UnderDevelopment/Tic Tac Toe/tic.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class tic{
     public static void main(String[] args){
-        System.out.println("Welcome to Tic Tac Toe! \n      A    |    B    |    C    \n_____________________________\n\n1\n\n\n2\n\n\n3");
+        System.out.println("Welcome to Tic Tac Toe! \n      A    |    B    |    C    \n   ________|_________|_______\n           |         |\n1          |         |\n   ________|_________|_______\n           |         |\n2          |         |\n   ________|_________|_______\n           |         | \n3          |         |\n           |         |       ");
     }
 }
