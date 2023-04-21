ed4516280ca3433de8bf39de43ec9f92c01f3e80
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-22 00:39:36 +0530

    Create gui.java

---
+++ UnderDevelopment/GUIForLogjn/gui.java
@@ -0,0 +1,24 @@
+package UnderDevelopment.GUIForLogjn;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class gui extends JFrame
+{
+    public static Jlabel directionsLabel
+    public static void main(String[] args)
+    {
+
+        gui window = new gui();
+        window.setSize(500,500);
+        window.setVisible(true);
+        window.setTitle("Login Window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+    }
+}
