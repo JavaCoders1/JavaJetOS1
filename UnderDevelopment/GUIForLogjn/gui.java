cbca7deae4a760028c4941e6b019d208aaccde1d
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-27 17:08:22 +0530

    gui for login page updated and ready to be used

--- UnderDevelopment/GUIForLogjn/gui.java
+++ UnderDevelopment/GUIForLogjn/gui.java
@@ -13,28 +13,34 @@ public class gui extends JFrame implements ActionListener
 
     gui() {
         setTitle("Login Screen");
-        setSize(300, 150);
+        setSize(260, 150);
         setLocationRelativeTo(null);
         setResizable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel = new JPanel(new GridLayout(3, 2));
+        panel = new JPanel();
+        panel.setLayout(null);
         add(panel);
 
         userlabel = new JLabel("Enter username: ");
+        userlabel.setBounds(10,10,110,25);
         panel.add(userlabel);
 
         usernamebox = new JTextField(10);
+        usernamebox.setBounds(125,10,120,25);
         panel.add(usernamebox);
 
         passlabel = new JLabel("Enter password: ");
+        passlabel.setBounds(10,40,120,25);
         panel.add(passlabel);
 
         passbox = new JTextField(10);
+        passbox.setBounds(125,40,120,25);
         panel.add(passbox);
 
         loginbutton = new JButton("Login");
         loginbutton.addActionListener((ActionListener) this);
+        loginbutton.setBounds(100,90,90,25);
         panel.add(loginbutton);
 
         setVisible(true);
---
+++ UnderDevelopment/GUIForMenu.java/menu.java
@@ -0,0 +1,9 @@
+package UnderDevelopment.GUIForMenu.java;
+
+import javax.swing.*;
+import java.awt.*;
+import java.
+
+public class menu {
+    
+}
