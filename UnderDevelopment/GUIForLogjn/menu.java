e89017bcb44c70e4e528779544bcbc51afe3232e
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-27 22:32:02 +0530

    pushing
    Co-authored-by: aayush.alimchandani@outlook.com <aayush.alimchandani@outlook.com>

--- UnderDevelopment/GUIForLogjn/menu.java
+++ UnderDevelopment/GUIForLogjn/menu.java
@@ -6,10 +6,18 @@ import java.awt.event.*;
 
 public class menu extends JFrame implements ActionListener
 {
-    JPanel menuWIndow;
+    JPanel menuWin;
     JLabel searchlabel;
     static JTextField searchbar;
-    menu(){
+    JButton textEditor, calc, ageCalc, numGame, monExp;
+    String[] apps = new String[10];
+    apps[0] = "1"; // Agecalc
+    apps[1] = "1"; // Calculator
+    apps[2] = "1"; // Monthly Expenditure
+    apps[3] = "1"; // Number Guessing Game
+    apps[4] = "1"; // Text Editor
+    menu()
+    {
         setTitle("Menu");
         setSize(400, 275);
         setLocationRelativeTo(null);
@@ -17,33 +25,20 @@ public class menu extends JFrame implements ActionListener
         
         setResizable(false);
 
-        menuWIndow = new JPanel();
-        menuWIndow.setLayout(null);
-        add(menuWIndow);
+        menuWin = new JPanel();
+        menuWin.setLayout(null);
+        add(menuWin);
 
         searchlabel = new JLabel("Enter Application Name:");
         searchlabel.setBounds(10,205,200,25);
-        menuWIndow.add(searchlabel);
+        menuWin.add(searchlabel);
 
         searchbar = new JTextField();
         searchbar.setBounds(175, 205, 120, 25);
-        menuWIndow.add(searchbar);
+        menuWin.add(searchbar);
 
-
-        setVisible(true);
-    }
-
-    public static void main(String[] args)
-    {
-        
-        JButton files, calc, ageCalc, numGame, monExp;
-        boolean run = true;
-        new menu();
-        while (run == true){
-            String searchText = searchbar.getText();
-            
-        files = new JButton("Text Editor");
-        files.addActionListener(this);
+        textEditor = new JButton("Text Editor");
+        textEditor.addActionListener(this);
 
         calc = new JButton("Calculator");
         calc.addActionListener(this);
@@ -56,6 +51,40 @@ public class menu extends JFrame implements ActionListener
 
         monExp = new JButton("Monthly Expenditure");
         monExp.addActionListener(this);
+
+        if (apps[0].equals("0")){
+            menuWin.remove(ageCalc);
+        }
+        if (apps[1].equals("0")){
+            menuWin.remove(calc);
+        }
+        if (apps[2].equals("0")){
+            menuWin.remove(monExp);
+        }
+        if (apps[3].equals("0")){
+            menuWin.remove(numGame);
+        }
+        if (apps[4].equals("0")){
+            menuWin.remove(textEditor)
+        }
+
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        
+        
+        boolean run = true;
+        new menu();
+        while (run == true){
+            String searchText = searchbar.getText();
+            apps[0] = "0";
+            if (!"Age Calculator".startsWith(searchText)){
+                apps[0] = "1;"
+            }
+               
             
         }
     }
