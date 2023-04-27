96ed1185434db23e0811d84fd11b6a7a9d906773
AFK190 <aayush.alimchandani@gmail.com>
2023-04-27 22:55:04 +0530

    e

--- UnderDevelopment/GUIForLogjn/menu.java
+++ UnderDevelopment/GUIForLogjn/menu.java
@@ -10,12 +10,11 @@ public class menu extends JFrame implements ActionListener
     JLabel searchlabel;
     static JTextField searchbar;
     JButton textEditor, calc, ageCalc, numGame, monExp;
-    String[] apps = new String[10];
-    apps[0] = "1"; // Agecalc
-    apps[1] = "1"; // Calculator
-    apps[2] = "1"; // Monthly Expenditure
-    apps[3] = "1"; // Number Guessing Game
-    apps[4] = "1"; // Text Editor
+    String apps0 = "1"; // Agecalc
+    String apps1 = "1"; // Calculator
+    String apps2 = "1"; // Monthly Expenditure
+    String apps3 = "1"; // Number Guessing Game
+    String apps4 = "1"; // Text Editor
     menu()
     {
         setTitle("Menu");
@@ -52,37 +51,40 @@ public class menu extends JFrame implements ActionListener
         monExp = new JButton("Monthly Expenditure");
         monExp.addActionListener(this);
 
-        if (apps[0].equals("0")){
+        if (apps0.equals("0")){
             menuWin.remove(ageCalc);
         }
-        if (apps[1].equals("0")){
+        if (apps1.equals("0")){
             menuWin.remove(calc);
         }
-        if (apps[2].equals("0")){
+        if (apps2.equals("0")){
             menuWin.remove(monExp);
         }
-        if (apps[3].equals("0")){
+        if (apps3.equals("0")){
             menuWin.remove(numGame);
         }
-        if (apps[4].equals("0")){
-            menuWin.remove(textEditor)
+        if (apps4.equals("0")){
+            menuWin.remove(textEditor);
         }
 
 
         setVisible(true);
     }
 
-    public static void main(String[] args)
-    {
-        
+    public static void main(String[] args) {
+        String apps0 = args[0];
+        String apps1 = args[1];
+        String apps2 = args[2];
+        String apps3 = args[3];
+        String apps4 = args[4];        
         
         boolean run = true;
         new menu();
         while (run == true){
             String searchText = searchbar.getText();
-            apps[0] = "0";
-            if (!"Age Calculator".startsWith(searchText)){
-                apps[0] = "1;"
+            // apps0 = "0";
+            if(!"Age Calculator".startsWith(searchText)){
+                apps0 = "0";
             }
                
             
