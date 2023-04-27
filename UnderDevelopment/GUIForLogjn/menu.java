c3e3fbf60e88445824f046dff05657accfd9b5bb
AFK190 <aayush.alimchandani@gmail.com>
2023-04-27 17:57:28 +0530

    Co-authored-by: DarkCoder-14 <DarkCoder-14@users.noreply.github.com>

--- UnderDevelopment/GUIForLogjn/menu.java
+++ UnderDevelopment/GUIForLogjn/menu.java
@@ -4,10 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class menu extends JFrame{
+public class menu extends JFrame implements ActionListener
+{
     JPanel panel1;
     JLabel searchlabel;
     JTextField searchbar;
+    JButton searchb;
     menu(){
         setTitle("Menu");
         setSize(400, 300);
@@ -19,12 +21,18 @@ public class menu extends JFrame{
         add(panel1);
 
         searchlabel = new JLabel("Enter Application Name:");
-        searchlabel.setBounds(10,205,160,25);
+        searchlabel.setBounds(10,205,200,25);
         panel1.add(searchlabel);
 
         searchbar = new JTextField();
         searchbar.setBounds(175, 205, 120, 25);
         panel1.add(searchbar);
+
+        searchb = new JButton("Search");
+        searchb.addActionListener(this);
+        searchb.setBounds(200, 235, 100,25);
+        panel1.add(searchb);
+
 
         setVisible(true);
     }
@@ -33,4 +41,5 @@ public class menu extends JFrame{
     {
         new menu();
     }
+
 }
