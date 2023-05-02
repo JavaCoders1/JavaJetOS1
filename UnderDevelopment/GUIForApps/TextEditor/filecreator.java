f3246d858d1cfe61154c83cc879220c7135fd5bb
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-05-02 11:08:24 +0530

    texteditor working now

--- UnderDevelopment/GUIForApps/TextEditor/filecreator.java
+++ UnderDevelopment/GUIForApps/TextEditor/filecreator.java
@@ -1,4 +1,5 @@
 package TextEditor;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,25 +7,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-
-
-
-public class filecreator extends JFrame implements ActionListener
-{
+public class filecreator extends JFrame implements ActionListener {
     JPanel cpanel;
     JButton createfile, save;
     JLabel filename, textentry;
-    JTextField filenamefield,textentryfield;
+    JTextField filenamefield, textentryfield;
 
-
-    filecreator()
-    {
-        
+    filecreator() {
         setTitle("Create File");
-        setSize(500,300);
+        setSize(500, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);    
+        setLocationRelativeTo(null);
         setResizable(false);
 
         cpanel = new JPanel();
@@ -32,65 +25,62 @@ public class filecreator extends JFrame implements ActionListener
         cpanel.setLayout(null);
 
         filename = new JLabel("File Name:");
-        filename.setBounds(15,15,70,25);
+        filename.setBounds(15, 15, 70, 25);
         cpanel.add(filename);
 
         filenamefield = new JTextField();
-        filenamefield.setBounds(90, 15,150, 25);
+        filenamefield.setBounds(90, 15, 150, 25);
         cpanel.add(filenamefield);
 
         textentry = new JLabel("Enter Text");
-        textentry.setBounds(15,100,100,25);
+        textentry.setBounds(15, 100, 100, 25);
         cpanel.add(textentry);
 
         textentryfield = new JTextField();
-        textentryfield.setBounds(120,100, 220, 100);
+        textentryfield.setBounds(120, 100, 220, 100);
         cpanel.add(textentryfield);
 
         save = new JButton("Save");
         save.addActionListener(this);
-        save.setBounds(345,100,100,25);
+        save.setBounds(345, 100, 100, 25);
         cpanel.add(save);
 
         createfile = new JButton("Create File");
         createfile.addActionListener(this);
-        createfile.setBounds(270,15,100,25);
+        createfile.setBounds(270, 15, 100, 25);
         cpanel.add(createfile);
 
         setVisible(true);
-
     }
-    public void actionPerformed(ActionEvent ae)
-    {
+
+    public void actionPerformed(ActionEvent ae) {
         String filename1 = filenamefield.getText();
-        if(ae.getSource()== createfile)
-        {
-            
-                File file = new File(filename1);
+        File file = new File(filename1);
+
+        if (ae.getSource() == createfile) {
+            try {
                 if (file.createNewFile()) {
-                    JOptionPane.showMessageDialog(this,"File created: " + file.getName());
+                    JOptionPane.showMessageDialog(this, "File created: " + file.getName());
                 } else {
-                    JOptionPane.showMessageDialog(this,"File already exists.");
+                    JOptionPane.showMessageDialog(this, "File already exists.");
                 }
-
-                if(ae.getSource()== save)
-                {String text = textentryfield.getText();
-
+            } catch (HeadlessException | IOException e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == save) {
+            String text = textentryfield.getText();
+            try {
                 FileWriter myWriter = new FileWriter(file);
                 myWriter.write(text);
                 myWriter.close();
-                System.out.println("Successfully wrote to the file.");
-            } 
+                JOptionPane.showMessageDialog(this, "Successfully wrote to the file.");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-                
-       
-            
         }
-        public static void main(String[] args)
-    {
+    }
+
+    public static void main(String[] args) {
         new filecreator();
     }
-    }
-
-    
-
+}
